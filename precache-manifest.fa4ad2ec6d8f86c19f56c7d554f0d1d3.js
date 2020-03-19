9dc393b94949b47ab96ee13c77f78bdcd4874c5a
s3665959 <s3665959@gmail.com>
2020-03-19 16:02:06 +0700

    Updates

---
+++ precache-manifest.fa4ad2ec6d8f86c19f56c7d554f0d1d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eb8d8a5a39a58da621f9ba5e724b4d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b23450f1edc5ee20081",
+    "url": "/static/css/main.03357466.chunk.css"
+  },
+  {
+    "revision": "98e44f3529eaa210dcbd",
+    "url": "/static/js/2.4ab9473f.chunk.js"
+  },
+  {
+    "revision": "c310a938bc930240e265a9711b814ed3",
+    "url": "/static/js/2.4ab9473f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b23450f1edc5ee20081",
+    "url": "/static/js/main.c1f808a4.chunk.js"
+  },
+  {
+    "revision": "8963f78c0e1a10b5862f",
+    "url": "/static/js/runtime-main.8b07e775.js"
+  }
+]);
